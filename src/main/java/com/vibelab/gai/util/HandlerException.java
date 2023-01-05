f2245e8b36cadac7bef4be85cd21de780ba4370c
Konstantin Nikolaenko <nikolaenko.kostya.vuz@mail.ru>
2023-01-06 01:30:10 +0300

    fix

--- src/main/java/com/vibelab/gai/util/HandlerException.java
+++ src/main/java/com/vibelab/gai/util/HandlerException.java
@@ -2,9 +2,7 @@ package com.vibelab.gai.util;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 
-//@Component
 public class HandlerException {
 
     public ResponseEntity<FineErrorResponse> handleException(FineBadRequestException ex){
